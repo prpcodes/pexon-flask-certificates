46e5c36f093653d349a220d1032077e8ce7ef9ba
prpcodes <work@prp.codes>
2023-02-14 04:24:05 +0100

    added  data-act-class="outline"

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
                 </div>
                 <div className="dropdown-content bg-base-200 text-base-content rounded-t-box rounded-b-box top-px max-h-96 h-[70vh] w-52 overflow-y-auto shadow-2xl mt-16 scrollbar-hide">
                     <div className="grid grid-cols-1 gap-3 p-3" tabIndex={0}>
-                        <div className="outline-base-content overflow-hidden rounded-lg outline-2 outline-offset-2 hover:outline" data-set-theme="dark">
+                        <div data-act-class="outline" className="outline-base-content overflow-hidden rounded-lg outline-2 outline-offset-2 hover:outline" data-set-theme="dark">
                             <div data-theme="dark" className="bg-base-100 text-base-content w-full cursor-pointer font-sans">
                                 <div className="grid grid-cols-5 grid-rows-3">
                                     <div className="col-span-5 row-span-3 row-start-1 flex gap-1 py-3 px-4">
