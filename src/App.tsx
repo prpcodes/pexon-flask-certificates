d1152f1ae3072ff7bf7dd68183f87f1bfbc4e810
prpcodes <work@prp.codes>
2023-02-14 03:05:11 +0100

    refactored hero content into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
 import { themeChange } from 'theme-change';
 import Footer from './components/Footer';
-import Hero from './components/Hero';
 import List from './components/List';
 import Navbar from './components/Navbar';
+import Card from './components/Card';
+import CertificationsList from './components/CertificationsList';
+import ChatBubbleStart from './components/ChatBubbleStart';
+import Form from './components/Form';
 
 function App() {
     // daisyUI theme changer
@@ -11,10 +14,34 @@ function App() {
         themeChange(false);
     }, []);
 
+    const [data, setData] = useState<Certification[]>(Object());
+
+    useEffect(() => {
+        setData([
+            { id: '1', name: 'AWS Cloud Practitioner' },
+            { id: '1', name: 'AWS Developer Associate' },
+            { id: '1', name: 'AWS Solutions Architect Associate' },
+            { id: '1', name: 'AWS SysOps Administrator Associate' },
+        ]);
+    }, []);
+
     return (
         <div className="App h-screen flex flex-col justify-between ali">
             <Navbar />
-            <Hero />
+            <div className="flex flex-col items-center justify-center">
+                <div className="flex flex-col items-start justify-center w-full md:max-w-md	 p-4">
+                    <ChatBubbleStart>
+                        <h1 className="text-3xl font-bold">
+                            Hello Pexonian <span className="wave">👋</span>
+                        </h1>
+                    </ChatBubbleStart>
+                    <ChatBubbleStart>
+                        <p className="text-xl ">Hier kannst du deine Zertifizierungen eintragen und speichern.</p>
+                    </ChatBubbleStart>
+                    <Form />
+                    <CertificationsList {...data} />
+                </div>
+            </div>
             <Footer />
         </div>
     );
