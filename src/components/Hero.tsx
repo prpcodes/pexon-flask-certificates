06611c21b0e3abf2ae8a94c7813a838ceebd5639
prpcodes <work@prp.codes>
2023-02-14 01:54:46 +0100

    added delete button to the list and fixed widths

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import List from './List';
 export default function Hero({ data = ['AWS Cloud Practitioner'] }: { data: string[] }) {
     return (
         <div className="flex flex-col items-center justify-center">
-            <div className="flex flex-col items-start justify-center w-96 p-4">
+            <div className="flex flex-col items-start justify-center w-full md:max-w-md	 p-4">
                 <ChatBubbleStart>
                     <h1 className="text-3xl font-bold">
                         Hello Pexonian <span className="wave">👋</span>
@@ -15,9 +15,9 @@ export default function Hero({ data = ['AWS Cloud Practitioner'] }: { data: stri
                     <p className="text-xl ">Hier kannst du deine Zertifizierungen eintragen und speichern.</p>
                 </ChatBubbleStart>
                 <Card>
-                    <form action="/members" method="POST">
-                        <input type="text" placeholder="AWS Cloud Practitioner" className="input input-bordered input-primary w-full max-w-xs" />
-                        <button type="submit" className="btn btn-primary mt-4">
+                    <form action="/members" method="POST" className="flex flex-col">
+                        <input type="text" placeholder="AWS Cloud Practitioner" className="input input-bordered input-primary w-full" />
+                        <button type="submit" className="btn btn-primary mt-4 w-max">
                             Speichern
                         </button>
                     </form>
@@ -27,7 +27,12 @@ export default function Hero({ data = ['AWS Cloud Practitioner'] }: { data: stri
                         <List>
                             {data.map((item: string, index: number) => {
                                 // display json data
-                                return <li key={index}>{item}</li>;
+                                return (
+                                    <li key={index} className="flex flex-row items-center justify-between w-full p-2">
+                                        <span className="text-md">{item}</span>
+                                        <button className="btn btn-sm btn-error">X</button>
+                                    </li>
+                                );
                             })}
                         </List>
                     </Card>
