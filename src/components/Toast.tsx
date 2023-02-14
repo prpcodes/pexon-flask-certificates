15d37020d428eaaf4c9a6dd7d6bb3812ccc6fd3a
prpcodes <work@prp.codes>
2023-02-14 08:26:24 +0100

    added icons

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -2,24 +2,24 @@ import toast from 'react-hot-toast';
 
 export function successToast(message: string) {
     return toast.custom((t) => (
-        <div className={`${t.visible ? 'animate-enter' : 'animate-leave'} max-w-xs w-full flex  alert alert-success`}>
-            <div className="flex-1 w-0 m-auto">{message}</div>
+        <div className={`${t.visible ? 'animate-enter' : 'animate-leave'} max-w-sm w-fit flex alert alert-success`}>
+            <div className="flex-1 w-full m-auto">✅ {message}</div>
         </div>
     ));
 }
 
 export function errorToast(message: string) {
     return toast.custom((t) => (
-        <div className={`${t.visible ? 'animate-enter' : 'animate-leave'} max-w-xs w-full flex alert alert-error`}>
-            <div className="flex-1 w-0 m-auto">{message}</div>
+        <div className={`${t.visible ? 'animate-enter' : 'animate-leave'} max-w-sm w-fit flex alert alert-error`}>
+            <div className="flex-1 w-full m-auto">⛔ {message}</div>
         </div>
     ));
 }
 
 export function infoToast(message: string) {
     return toast.custom((t) => (
-        <div className={`${t.visible ? 'animate-enter' : 'animate-leave'} max-w-xs w-full flex alert alert-info`}>
-            <div className="flex-1 w-0 m-auto">{message}</div>
+        <div className={`${t.visible ? 'animate-enter' : 'animate-leave'} max-w-sm w-fit flex alert alert-info`}>
+            <div className="flex-1 w-0 m-auto">ℹ️ {message}</div>
         </div>
     ));
 }
