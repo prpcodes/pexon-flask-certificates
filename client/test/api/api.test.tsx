59d4d970f560622ebf476f10192eb325912a1f84
prpcodes <work@prp.codes>
2023-02-15 09:24:39 +0100

    refactored test to leave no garbage

--- client/test/api/api.test.tsx
+++ client/test/api/api.test.tsx
@@ -1,16 +1,19 @@
 import { describe, expect, test } from 'vitest';
 
+// These tests are expected to run in the same order every time,
+// so they can use the same cert object and no garbage is left in the database after the tests.
+
 describe('API', () => {
+    let cert: any;
     test('gets all certifications', async () => {
         // fetch all certifications from the API
-        const data = await fetch('http://localhost:5000/api/certifications').then((response) => response.json());
+        const res = await fetch('http://localhost:5000/api/certifications').then((response) => response.json());
         // assert that the response is an object with a certifications array
-        expect(data).toHaveProperty('certifications');
+        expect(res).toHaveProperty('certifications');
     });
 
     test('creates a new certification', async () => {
-        // create a new certification
-        const data = await fetch('http://localhost:5000/api/certification', {
+        cert = await fetch('http://localhost:5000/api/certification', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -21,35 +24,23 @@ describe('API', () => {
         }).then((response) => response.json());
 
         // assert that the response is an object with a certification object
-        expect(data).toHaveProperty('certification');
-        expect(data.certification).toHaveProperty('id');
-        expect(data.certification).toHaveProperty('name');
-        expect(data.certification).toHaveProperty('created_at');
-        expect(data.certification.name).toBe('Test Certification');
+        expect(cert).toHaveProperty('certification');
+        expect(cert.certification).toHaveProperty('id');
+        expect(cert.certification).toHaveProperty('name');
+        expect(cert.certification).toHaveProperty('created_at');
+        expect(cert.certification.name).toBe('Test Certification');
     });
 
-    test('deletes a certification', async () => {
-        // create a new certification
-        const data = await fetch('http://localhost:5000/api/certification', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name: 'Test Certification',
-            }),
-        }).then((response) => response.json());
-
-        // delete the certification
-        const deleted = await fetch(`http://localhost:5000/api/certification/${data.certification.id}`, {
+    test('creates and deletes a certification', async () => {
+        const res = await fetch(`http://localhost:5000/api/certification/${cert.certification.id}`, {
             method: 'DELETE',
         }).then((response) => response.json());
 
         // assert that the response is an object with a certification object
-        expect(deleted).toHaveProperty('certification');
-        expect(deleted.certification).toHaveProperty('id');
-        expect(deleted.certification).toHaveProperty('name');
-        expect(deleted.certification).toHaveProperty('created_at');
-        expect(deleted.certification.name).toBe('Test Certification');
+        expect(res).toHaveProperty('certification');
+        expect(res.certification).toHaveProperty('id');
+        expect(res.certification).toHaveProperty('name');
+        expect(res.certification).toHaveProperty('created_at');
+        expect(res.certification.name).toBe('Test Certification');
     });
 });
